c9d6fae16348a9b78bfa29b00f45b3dafdf42806
Viji <vijme@yahoo.com>
2014-10-14 11:22:59 -0400

    Modified for login from sencha

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
+  resources :registers
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
    root 'welcome#index'
+
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
@@ -13,7 +16,11 @@ Rails.application.routes.draw do
 
   # Example resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
+   resources :login
+  #match "/check/:email/:password", :to => "registers#check", :via => [:get, :post]
+  match "/check", :to => "registers#check", :via => [:get, :post]
 
+  #match "/register" , :to => "registers#create", :via => [:get, :post]
   # Example resource route with options:
   #   resources :products do
   #     member do
